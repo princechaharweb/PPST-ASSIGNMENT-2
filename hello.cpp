342427ef7436733eff7fa769e13d956e4600dc92
Prince Chahar <princechahar456@gmail.com>
2026-02-22 16:08:35 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+// factorial of a number
+#include <iostream>
+using namespace std ;
+int main (){
+    int i, n, fact = 1 ;
+    cout << "Enter a number: " ;
+    cin >> n ;
+    for(i = 1; i <= n; i++){
+        fact = fact * i ;
+    }
+    cout << "Factorial of " << n << " is " << fact ;
+    return 0 ;
+
+}
