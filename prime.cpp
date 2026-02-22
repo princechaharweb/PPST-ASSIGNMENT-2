c153e91bde548df159f954127b738f8b04314cf4
Prince Chahar <princechahar456@gmail.com>
2026-02-22 16:13:26 +0530

    Prime check

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+// whether a number is prime or not
+#include <iostream>
+using namespace std ;
+int main (){
+    int n, i, flag = 0 ;
+    cout << "Enter a number: " ;
+    cin >> n ;
+    for(i = 2; i <= n/2; i++){
+        if(n % i == 0){
+            flag = 1 ;
+            break ;
+        }
+    }
+    if(flag == 0)
+        cout << n << " is a prime number" ;
+    else
+        cout << n << " is not a prime number" ;
+    return 0 ;
+
+}
